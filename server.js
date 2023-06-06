6d1a215e35c56c58b565b2604bd79ac43353fadc
ortizfram <ortizfranco48@gmail.com>
2023-06-06 14:03:42 -0300

    1517

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const articleRouter = require("./routes/articles");
 
 app.set("view engine", "ejs");
 
+app.use("/articles", articleRouter);
+
 app.get("/", (req, res) => {
   // res.send("Hello world")
-  res.render("index");
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: Date.now(),
+      description: "Test description...",
+    },
+    {
+      title: "Test Article 2",
+      createdAt: Date.now(),
+      description: "Test description...",
+    },
+  ];
+  res.render("index", { articles: articles });
 });
 
 app.listen(5000);
